7c7f56d69293baa7d330f17ef6c4273be80df564
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2024-11-03 12:25:15 +0530

    Update rms.py

--- BASE/Components/rms.py
+++ BASE/Components/rms.py
@@ -1,10 +1,3 @@
-"""
-Restaurant Management System v0.1.2 NA,
-    App is made to facilitate restaurant management processes.
-
-Developed by Aliyev Nurlan in Dec 2022
-    Last upgrades: Jan 2023
-"""
 
 from mainwindow import MainWindow
 from ctypes import windll
